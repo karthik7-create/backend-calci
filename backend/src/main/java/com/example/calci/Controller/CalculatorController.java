0bbc0cafd808835a6c31aa7b46655e8b4b2cc08e
Karthik <karthikkeyan3118@gmail.com>
2026-03-08 00:06:45 +0530

    frontend vercel integration

--- backend/src/main/java/com/example/calci/Controller/CalculatorController.java
+++ backend/src/main/java/com/example/calci/Controller/CalculatorController.java
@@ -12,7 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins="https://minicalci.vercel.app")
 public class CalculatorController {
 
     @Autowired
